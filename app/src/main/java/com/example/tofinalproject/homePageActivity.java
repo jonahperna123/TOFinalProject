2af8ea51180a92388c4675fdab2f4391d4d48d3e
Thais Gonzalez <thaisgm@umich.edu>
2019-11-19 13:28:27 -0500

    Added basic&tempo navmenu; API call to TMDB on socialActivity

--- app/src/main/java/com/example/tofinalproject/homePageActivity.java
+++ app/src/main/java/com/example/tofinalproject/homePageActivity.java
@@ -1,8 +1,13 @@
 package com.example.tofinalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class homePageActivity extends AppCompatActivity {
 
@@ -10,5 +15,48 @@ public class homePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navmenu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.itemLogin) {
+            Intent loginIntent = new Intent(this, loginActivity.class);
+            startActivity(loginIntent);
+
+        } else if (item.getItemId() == R.id.itemHome) {
+            Intent homeIntent = new Intent(this, homePageActivity.class);
+            startActivity(homeIntent);
+
+        } else if (item.getItemId() == R.id.itemMessage) {
+            Intent messageIntent = new Intent(this, MessageActivity.class);
+            startActivity(messageIntent);
+
+        } else if (item.getItemId() == R.id.itemMovie) {
+            Intent movieIntent = new Intent(this, MovieActivity.class);
+            startActivity(movieIntent);
+
+        } else if (item.getItemId() == R.id.itemReview) {
+            Intent reviewIntent = new Intent(this, ReviewMovieActivity.class);
+            startActivity(reviewIntent);
+
+        } else if (item.getItemId() == R.id.itemSocial) {
+            Intent socialIntent = new Intent(this, SocialActivity.class);
+            startActivity(socialIntent);
+
+        } else if (item.getItemId() == R.id.itemProfile) {
+            Intent profileIntent = new Intent(this, ViewProfileActivity.class);
+            startActivity(profileIntent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
