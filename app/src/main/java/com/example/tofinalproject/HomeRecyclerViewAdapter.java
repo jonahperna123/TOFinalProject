ea29f99b7a5537ebcf1b419b714c1dba255ae959
rebtung <rtung@umich.edu>
2019-12-15 17:16:01 -0500

    Mainly UI change

--- app/src/main/java/com/example/tofinalproject/HomeRecyclerViewAdapter.java
+++ app/src/main/java/com/example/tofinalproject/HomeRecyclerViewAdapter.java
@@ -28,15 +28,18 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.homepage_item, parent, false);
-        HomeRecyclerViewAdapter.ViewHolder viewHolder = new ViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.rating.setRating(reviews.get(position).starRating);
-        holder.textViewReview.setText(reviews.get(position).review);
-        holder.textViewUserEmail.setText(reviews.get(position).userEmail);
+        Review review = reviews.get(position);
+
+        holder.rating.setRating(review.starRating);
+        holder.movieTitle.setText(review.contentTitle);
+        holder.review.setText(review.review);
+        holder.username.setText(review.userEmail);
     }
 
     @Override
@@ -45,24 +48,18 @@ public class HomeRecyclerViewAdapter extends RecyclerView.Adapter<HomeRecyclerVi
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
-        ImageView imageViewAvatar, imageViewMoviePoster;
-        TextView textViewUserEmail, textViewReview;
+        ImageView avatar;
+        TextView username, movieTitle, review;
         RatingBar rating;
-        RelativeLayout parentLayout, userLayout, reviewLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            imageViewAvatar = itemView.findViewById(R.id.imageViewAvatar_HomePageActivity);
-            imageViewMoviePoster = itemView.findViewById(R.id.imageViewMoviePoster_HomePageActivity);
-            textViewUserEmail = itemView.findViewById(R.id.textViewEmail_HomePageActivity);
-            textViewReview = itemView.findViewById(R.id.textViewReview_HomePageActivity);
+            avatar = itemView.findViewById(R.id.imageViewAvatar_HomePageActivity);
+            username = itemView.findViewById(R.id.textViewUsername_HomePageActivity);
+            movieTitle = itemView.findViewById(R.id.textViewMovieTitle_HomePageActivity);
+            review = itemView.findViewById(R.id.textViewReview_HomePageActivity);
             rating = itemView.findViewById(R.id.ratingBar_HomePageActivity);
-            parentLayout = itemView.findViewById(R.id.layout_HomePageActivity);
-            userLayout = itemView.findViewById(R.id.userLayout_HomePageActivity);
-            reviewLayout = itemView.findViewById(R.id.reviewLayout_HomePageActivity);
-
         }
     }
 }
